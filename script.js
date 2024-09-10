c71892ededf7b7d8e1ad531ce9593e112198812e
DonTranDev <145828911+DonTranDev@users.noreply.github.com>
2024-09-09 17:17:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ accordionButtons.forEach(button => {
 
 // Banner Animation
 let bannerIndex = 0;
-const banners = ['Untitled design.png', 'banner2.jpg', 'banner3.jpg'];
+const banners = ['CollageBanner.png', 'banner2.jpg', 'banner3.jpg'];
 const bannerElement = document.querySelector('.banner');
 
 function switchBanner() {
